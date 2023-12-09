2694bf48a867db5d3ba0e809819af304ae9735bf
EduardoHenriqueB <eduardohenrique8148@gmail.com>
2023-12-09 00:59:13 -0300

    create initial quiz

--- pages/quiz.js
+++ pages/quiz.js
@@ -1,29 +1,57 @@
+// pages/quiz.js
 import React, { useState } from 'react';
 import styles from '../styles/quiz.module.css';
 
 const Quiz = () => {
   const [selectedOption, setSelectedOption] = useState(null);
+  const [isCorrect, setIsCorrect] = useState(null);
+  const [isConfirmed, setIsConfirmed] = useState(false);
 
-  const options = ['Opção A', 'Opção B', 'Opção C', 'Opção D'];
+  const question = 'O que é cyberbullying?';
+  const options = [
+    'É manipular psicologicamente pessoas para executar ações na Internet.',
+    'É expor publicamente dados pessoais de usuários da Internet.',
+    'É enganar pessoas na Internet e obter informações confidenciais.',
+    'É a violência intencional repetitiva praticada contra alguém, através da internet ou de outras tecnologias relacionadas ao mundo virtual.',
+  ];
+  const correctAnswerIndex = 3; // Defina o índice da resposta correta aqui
 
   const handleOptionClick = (index) => {
-    setSelectedOption(index);
+    if (!isConfirmed) {
+      setSelectedOption(index);
+    }
+  };
+
+  const handleConfirm = () => {
+    setIsConfirmed(true);
+    setIsCorrect(selectedOption === correctAnswerIndex);
   };
 
   return (
-    <div className={styles.quiz_container}>
-      <h2 className={styles.question_text}>Qual é a sua resposta?</h2>
-      <div className={styles.options_container}>
+    <div className={styles.quizContainer}>
+      <h1 className={styles.pergunta}>{question}</h1>
+      <div className={styles.optionsContainer}>
         {options.map((option, index) => (
           <div
             key={index}
-            className={`option ${selectedOption === index ? 'selected' : ''}`}
+            className={`${styles.option} ${
+              selectedOption === index ? styles.selected : ''
+            } ${isConfirmed && index === correctAnswerIndex ? styles.correct : ''} ${
+              isConfirmed && index === selectedOption && !isCorrect ? styles.incorrect : ''
+            }`}
             onClick={() => handleOptionClick(index)}
           >
             {option}
           </div>
         ))}
       </div>
+      <button
+        className={styles.confirmButton}
+        onClick={handleConfirm}
+        disabled={isConfirmed}
+      >
+        Confirmar
+      </button>
     </div>
   );
 };
